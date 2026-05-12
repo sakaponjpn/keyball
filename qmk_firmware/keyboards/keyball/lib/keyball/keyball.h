23e37520805ec6a6590da539310536a2ab7d9a4a
sakaponjpn <tatsuya.sakashita@gmail.com>
2026-05-12 16:22:09 +0900

    update auto mouse layer

--- qmk_firmware/keyboards/keyball/lib/keyball/keyball.h
+++ qmk_firmware/keyboards/keyball/lib/keyball/keyball.h
@@ -177,6 +177,10 @@ typedef struct {
     keypos_t       last_pos;
     report_mouse_t last_mouse;
 
+    uint16_t auto_mouse_layer_timeout;
+    layer_state_t last_layer_state;
+    uint16_t total_mouse_movement;
+
     // Buffer to indicate pressing keys.
     char pressing_keys[KEYBALL_OLED_MAX_PRESSING_KEYCODES + 1];
 } keyball_t;
